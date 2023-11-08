ce95a042f34dbbac5c74dc4fa1a5cf0790897a3f
Gaurav Agarwal <20bcs047@iiitdwd.ac.in>
2023-11-08 14:17:59 +0530

    handling state

--- client/src/components/TodoForm.jsx
+++ client/src/components/TodoForm.jsx
@@ -1,9 +1,23 @@
+import { useState } from "react";
+
 const TodoForm = () => {
+    const [text, setText] = useState("");
+
+    const onFormSubmit = () => {
+    }
+
+    const onInputChange = (e) => {
+        console.log(e.target.value);
+        setText(e.target.value);
+    }
+
     return (
-        <form className="form">
+        <form className="form" onSubmit={onFormSubmit}>
             <input  
                 placeholder="Enter new todo..."
                 className="input"
+                onChange={onInputChange}
+                value={text}
             />
         </form>
     )
